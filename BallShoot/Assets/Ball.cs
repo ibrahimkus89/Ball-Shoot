5825c72b8fb5696cde8004636171136dcca16931
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2023-03-14 13:00:09 +0300

    balls

--- BallShoot/Assets/Ball.cs
+++ BallShoot/Assets/Ball.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bucket"))
         {
             gameObject.transform.localPosition=Vector3.zero;
             gameObject.transform.localRotation=Quaternion.Euler(Vector3.zero);
+            rb.velocity=Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
             gameObject.SetActive(false);
             //parc eff
             //numbers update
@@ -20,6 +28,8 @@ public class Ball : MonoBehaviour
         {
             gameObject.transform.localPosition = Vector3.zero;
             gameObject.transform.localRotation = Quaternion.Euler(Vector3.zero);
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
             gameObject.SetActive(false);
         }
     }
